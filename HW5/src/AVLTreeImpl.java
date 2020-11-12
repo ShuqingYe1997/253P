ba833891ffd4fd42f16ce0af45a992b9ac018552
ShuqingYe1997 <yeshuqing1997@126.com>
2020-11-13 02:32:17 +0800

    modif: HW5 correct code

--- HW5/src/AVLTreeImpl.java
+++ HW5/src/AVLTreeImpl.java
@@ -84,24 +84,22 @@ class AVLTree{
         AVLNode rightTree = root.right;
         root.right = rightTree.left;
         rightTree.left = root;
-        root = rightTree;
 
         root.height = Math.max(height(root.left), height(root.right)) + 1;
         rightTree.height = Math.max(height(rightTree.left), height(rightTree.right)) + 1;
 
-        return root;
+        return rightTree;
     }
 
     private AVLNode rotateRight (AVLNode root) {
         AVLNode leftTree = root.left;
         root.left = leftTree.right;
         leftTree.right = root;
-        root = leftTree;
 
         root.height = Math.max(height(root.left), height(root.right)) + 1;
         leftTree.height = Math.max(height(leftTree.left), height(leftTree.right)) + 1;
 
-        return root;
+        return leftTree;
     }
 
     // return the height of a node
@@ -197,12 +195,15 @@ class AVLTree{
             else if (node.right == null)  // has left child
                 node = node.left;
             else {  // parent has 2 children
-                AVLNode minNode = deleteMinNode(node.right);  // get the min node in right subTree
-                node.val = minNode.val;  // copy min node's value
+//                AVLNode suc = getSuccessor(node.right);  // get the min node in right subTree
+//                node.val = suc.val;  // copy min node's value
+                AVLNode pre = getPredecessor(node.left);  // get the max node in left subTree
+                node.val = pre.val;
+                node.left = recursive_delete(node.left, pre.val);
             }
         }
 
-        // Then it can skip all the following activities...
+        // if node is null then it can skip all the following activities...
         if (node == null)
             return null;
 
@@ -235,16 +236,70 @@ class AVLTree{
         return node;
     }
 
-    private AVLNode deleteMinNode(AVLNode root) {
-        if (root == null)
+    // For deleting predecessor/successor of node under inorder traversal
+    private AVLNode recursive_delete(AVLNode node, int n) {
+        if (node == null)
             return null;
-        AVLNode minNode = null;
-        while (root.left != null && root.left.left != null)
-            root = root.left;
 
-        minNode = root.left;
-        root.left = null;
-        return minNode;
+        // 1. regular deletion
+        if (node.val > n)
+            node.left = recursive_delete(node.left, n);
+        else if (node.val < n)
+            node.right = recursive_delete(node.right, n);
+        else {  // hit
+            if (node.left == null && node.right == null) // parent has no child
+                node = null;
+            else if (node.left == null)  // has right child
+                node = node.right;
+            else if (node.right == null)  // has left child
+                node = node.left;
+            else {  // parent has 2 children
+                AVLNode pre = getPredecessor(node.left);  // get the max node in left subTree
+                node.val = pre.val;
+                node.left = recursive_delete(node.left, pre.val);
+            }
+        }
+
+        // if node is null then it can skip all the following activities...
+        if (node == null)
+            return null;
+
+        node.height = Math.max(height(node.left), height(node.right)) + 1;
+
+        int balance = getBalance(node);
+
+        // Left Left
+        if (balance > 1 && getBalance(node.left) >= 0)
+            return rotateRight(node);
+
+        // Right Right
+        if (balance < -1 && getBalance(node.right) <= 0)
+            return rotateLeft(node);
+
+        // Left Right
+        if (balance > 1 && getBalance(node.left) < 0)
+        {
+            node.left = rotateLeft(node.left);
+            return rotateRight(node);
+        }
+
+        // Right Left
+        if (balance < -1 && getBalance(node.right) > 0)
+        {
+            node.right = rotateRight(node.right);
+            return rotateLeft(node);
+        }
+
+        return node;
+    }
+
+    private AVLNode getPredecessor(AVLNode node) {
+        if (node == null)
+            return null;
+        while(node.right != null) {
+            node = node.right;
+        }
+        return node;
     }
 
     void printAll() {
@@ -268,31 +323,30 @@ public class AVLTreeImpl {
             Scanner scanner = new Scanner(System.in);
             String filename = scanner.nextLine();
             FileReader fileReader = new FileReader(new File("./" + filename +".txt"));
+
             BufferedReader bufferedReader = new BufferedReader(fileReader);
             String line = "";
             AVLTree avlTree = new AVLTree();
             while ((line = bufferedReader.readLine()) != null) {
-                System.out.println(line);
-            }
+                String[] commands = line.split("\\s+");
+                switch (commands[0]) {
+                    case "insert":
+                        avlTree.insert(Integer.parseInt(commands[1]));
+                        break;
+                    case "find":
+                        avlTree.find(Integer.parseInt(commands[1]));
+                        break;
+                    case "delete":
+                        avlTree.delete(Integer.parseInt(commands[1]));
+                        break;
+                    default:
+                        System.out.println("No such method! Please check your input.");
+                }
 
-//                String[] commands= line.split("\\s+");
-//                switch(commands[0]) {
-//                    case "insert":
-//                        avlTree.insert(Integer.parseInt(commands[1]));
-//                        break;
-//                    case "find":
-//                        avlTree.find(Integer.parseInt(commands[1]));
-//                        break;
-//                    case "delete":
-//                        avlTree.delete(Integer.parseInt(commands[1]));
-//                        break;
-//                    default:
-//                        System.out.println("No such method! Please check your input.");
-//                }
-//            }
-//
-//            // Final output
-//            avlTree.printAll();
+
+            }
+            // Final output
+            avlTree.printAll();
 
         }catch (IOException e) {
             e.printStackTrace();
